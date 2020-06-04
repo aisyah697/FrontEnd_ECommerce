63a3c0963e75f1e342c7e6b62dad2571c399b7e0
aisyah697 <aisyah@alterra.id>
2020-06-04 19:02:47 +0700

    add dockerfile

---
+++ build/precache-manifest.82e18239b524f071ee18dac6db42e1a5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de0197743d943e80f9e7ce0814131626",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abc867d4b27f9c4611ee",
+    "url": "/static/css/main.f0998ce6.chunk.css"
+  },
+  {
+    "revision": "d5e03f02be607829735f",
+    "url": "/static/js/2.fb752eee.chunk.js"
+  },
+  {
+    "revision": "41b66b6fd6eecba581340794bb1831be",
+    "url": "/static/js/2.fb752eee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abc867d4b27f9c4611ee",
+    "url": "/static/js/main.d016c24e.chunk.js"
+  },
+  {
+    "revision": "e6fae057a575f0aad8e6",
+    "url": "/static/js/runtime-main.acda1d89.js"
+  },
+  {
+    "revision": "5afd32e207f2be33d931a2b6fecad55c",
+    "url": "/static/media/banner3.5afd32e2.jpg"
+  },
+  {
+    "revision": "46429b0284a5d73226f2b6b479b93d3e",
+    "url": "/static/media/gif1.46429b02.gif"
+  },
+  {
+    "revision": "fd143f4af2e733c5c112fc38782e7c9f",
+    "url": "/static/media/logo.fd143f4a.jpg"
+  },
+  {
+    "revision": "533d5cb509bc9aa5d7395ab6e966254e",
+    "url": "/static/media/mini-banner-home.533d5cb5.jpg"
+  }
+]);
