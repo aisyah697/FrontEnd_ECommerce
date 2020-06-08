86c78d36cd65ce35636659d407a62e85434cfe65
aisyah697 <aisyah@alterra.id>
2020-06-08 11:53:23 +0700

    cicd2

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 
 
-sudo docker build -f "Dockerfile" -t ian18ishar/fe:$TRAVIS_BUILD_NUMBER .
-sudo docker push ian18ishar/fe:$TRAVIS_BUILD_NUMBER
+sudo docker build -f "Dockerfile" -t aisyah697/fe-ecommerce:$TRAVIS_BUILD_NUMBER .
+sudo docker push aisyah697/fe-ecommerce:$TRAVIS_BUILD_NUMBER
 
 echo $GCLOUD_SERVICE_KEY_PRD | base64 --decode > ${HOME}/gcloud-service-key.json
 gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
